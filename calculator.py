ff81459ef77ea978a8a64bce7551bb726a593bd1
Tommi Mosconi <tommi.mosconi@gmail.com>
2025-01-04 13:11:34 +0200

    first commit

---
+++ calculator.py
@@ -0,0 +1,4 @@
+monthly_pay = int(input("Your monthly support from the state (€): "))
+expected_fund_growth = int(input("Expected yearly grow of you fund (%): "))
+years_support = int(input("Lenght of your support from the state (years): "))
+real_interest_rate = int(input("Real interest rate of your loan (%): "))
